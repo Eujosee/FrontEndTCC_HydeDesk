ae8d5b0246e8bc6207c2d67c81d502a6aaa0c953
llucassilva <llucassilvap@gmail.com>
2023-02-08 08:24:45 -0300

    atualização home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -11,10 +11,11 @@ function Home() {
                 <div className="max-w-sm">
                     <h1 className="font-bold">Suporte</h1>
                     <h1 className="font-bold">Help Desk</h1>
-                    
+                        
                     <p className="semi-bold">Aliquam laoreet commodo laoreet. Aenean luctus neque ut risus volutpat interdum.
                     Phasellus tristique sem vel odio sollicitudin facilisis. Aliquam erat volutpat.</p>
 
+                    {/* botao redirecionando */}
                 <div className="mt-8 flex flex-col">
                   <Link to="/perfil">
                     <button className="hover:bg-cyan-600 mb-6 bg-azul-hyde p-2 rounded-3xl text-white font-bold text-lg"            
