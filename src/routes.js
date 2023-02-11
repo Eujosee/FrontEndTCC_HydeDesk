918c3b930bf529cb1674abdfe422b440a294c20b
Miguel Soares <98955643+MiguelSoaresDev@users.noreply.github.com>
2023-02-11 17:51:27 -0300

    mudar a senha pronta

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,8 @@ import CadastroFunc from "./pages/CadastroFunc";
 import Perfil from "./pages/Perfil";
 import RecuperarSenha from "./pages/RecuperaSenha";
 import Detalhes from "./pages/Detalhes";
+import ListaFunc from "./pages/ListaFunc";
+import MudaSenha from "./pages/MudaSenha";
 
 const Rotas = () => {
     return (
@@ -18,11 +20,13 @@ const Rotas = () => {
             <Route path="/" element={<Home/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/detalhes" element={<Detalhes/>}/>
-
             <Route path="/cadastro" element={<Cadastro/>}/>
             <Route path="/cadastro-funcionario" element={<CadastroFunc/>}/>
             <Route path="/perfil" element={<Perfil/>}/>
-            <Route path="/recuperar" element={<RecuperarSenha/>}/>\a
+            <Route path="/recuperar" element={<RecuperarSenha/>}/>
+            <Route path="/lista-funcionarios" element={<ListaFunc/>}/>
+            <Route path="/mudarsenha" element={<MudaSenha/>}/>
+
         </Routes>
         </BrowserRouter>
     </AuthProvider>
