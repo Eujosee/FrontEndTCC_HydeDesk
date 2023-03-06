2d48c15ceb47f3f88dfb0f750d2f309b3b2bdc5b
Taima <gustavotaima18@gmail.com>
2023-03-06 11:51:32 -0300

    Paginação terminada

--- src/components/PaginationButton/index.js
+++ src/components/PaginationButton/index.js
@@ -4,12 +4,12 @@ export default function PaginationButton({
   handleChangePage,
 }) {
   return (
-    <div className="flex flex-col w-12 h-12">
+    <div className="flex flex-col w-10 h-10">
       <button
         key={index}
         className={`p-2 h-full w-full text-base font-semibold text-black ${
           index === currentPage
-            ? "border-b-blue-600 border-b-4 rounded-sm"
+            ? "border-b-azul-hyde border-b-4 rounded-sm"
             : "hover:bg-slate-200 rounded-md"
         }`}
         onClick={() => handleChangePage(index)}
--- src/pages/ListaChamados/index.js
+++ src/pages/ListaChamados/index.js
@@ -373,20 +373,20 @@ function ListaChamados() {
           )}
         </div>
         {chamados?.length !== 0 && (
-          <div className="flex w-full justify-center p-2 mt-10">
-            <div className="flex align-center rounded-lg gap-5 justify-center px-3 py-2 shadow-lg">
-              <div>
+          <div className="flex w-full justify-center mt-10 mb-10">
+            <div className="flex rounded-lg justify-center px-2 py-2 shadow-lg w-11/12 md:w-1/2 lg:w-1/3">
+              <div className="flex align-center justify-center w-12 h-10">
                 <button
-                  className="flex align-center justify-center rounded-md p-2 h-full w-12 text-base font-semibold text-black hover:bg-slate-200"
+                  className="flex item-center justify-center rounded-md item-center h-full w-full text-base font-semibold text-black hover:bg-slate-200"
                   onClick={prevPage}
                 >
                   <ChevronLeftIcon
-                    className="h-7 w-7 flex-none"
+                    className="h-full w-7"
                     aria-hidden="true"
                   />
                 </button>
               </div>
-              <div className="flex flex-row h-full w-full gap-5 align-center">
+              <div className="flex flex-row w-full h-full px-2 justify-evenly" > 
                 {paginationButtons?.map((button, index) => {
                   if (currentPage < 2) {
                     if (index >= 0 && index < 5) {
@@ -426,13 +426,13 @@ function ListaChamados() {
                   }
                 })}
               </div>
-              <div>
+              <div className="flex align-center justify-center w-12 h-10">
                 <button
-                  className="flex align-center justify-center rounded-md p-2 h-full w-12 text-base font-semibold text-black hover:bg-slate-200"
+                  className="flex align-center justify-center rounded-md item-center h-full w-full text-base font-semibold text-black hover:bg-slate-200"
                   onClick={nextPage}
                 >
                   <ChevronRightIcon
-                    className="h-7 w-7 flex-none"
+                    className="h-full w-7"
                     aria-hidden="true"
                   />
                 </button>
