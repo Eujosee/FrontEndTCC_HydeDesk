49b9332ea84daeed2195dbcc4b9cfdc6d74afbdb
Theuszer4 <theus009007@outlook.com>
2023-03-08 11:27:42 -0300

    estreals

--- src/components/Estrelas/index.js
+++ src/components/Estrelas/index.js
@@ -1,48 +1,28 @@
-import React, { useState } from "react";
-import { FaStar } from "react-icons/fa";
-import { Container, Rating } from "./RatingStyles";
-const Rate = () => {
-    const [rate, setRate] = useState(0);
-    return (
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
 
-        <div>
-            <Container>
-                {[...Array(5)].map((item, index) => {
-                    const givenRating = index + 1;
-                    return (
-                        <label>
-                            <button
-                                type="radio"
-                                value={givenRating}
-                                onClick={() => {
-                                    setRate(givenRating);
+export default function BasicRating() {
+  const [value, setValue] = React.useState(2);
+  console.log(value)
 
-
-                                }}
-                            />
-                            <Rating>
-                                <FaStar
-                                    color={
-                                        givenRating < rate || givenRating === rate
-                                            ? "rgb(255 239 69)"
-                                            : "rgb(192,192,192)"
-                                    }
-                                />
-                            </Rating>
-                        </label>
-                    );
-                })}
-            </Container>
-            <div>
-                {rate < 3 && <div className="flex flex-col w-full">
-                    <label className="font-medium text-gray-500">
-                        Detalhes da Avaliação
-                    </label>
-                    <textarea className="bg-gray-300 rounded h-36 pt-2 pl-4 shadow" />
-                </div>}
-            </div>
-        </div>
-    );
-};
-
-export default Rate;
+  return (
+    <Box
+      sx={{
+        '& > legend': { mt: 2 },
+      }}
+    >
+      <Typography component="legend">Controlled</Typography>
+      <Rating
+        name="simple-controlled"
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      />
+     
+      
+    </Box>
+  );
+}
