a3b1dadf7b25ebc787f11b2755b73eed3a5b0583
Eujosee <josemotadossantosjunior@hotmail.com>
2023-04-09 22:42:55 -0300

    bug no dashboard arrumado

--- src/components/PieChart/index.jsx
+++ src/components/PieChart/index.jsx
@@ -1,40 +1,47 @@
 import { useEffect } from "react";
 import api from "../../services/api";
-import { Pie } from "react-chartjs-2";
+import { Doughnut, Pie } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { useState } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function PieChart() {
     const dataStatus = []
+    const [dataChart, setDataChart] = useState({})
     useEffect(() => {
         (async () => {
             const { data } = await api.get("/chamados")
             getStatusData(data)
+
+       
+                setDataChart({
+                    labels: ['Concluido', 'Pendente', 'Andamento', 'Cancelado'],
+                    datasets:[
+                        {
+                            label:'Nº de chamados',
+                            data: dataStatus,
+                            backgroundColor: [
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                            ],
+                            borderColor: [
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(54, 162, 235, 1)',
+                              ],
+                              borderWidth: 1,
+                        },
+                    ],
+                })
+
         })()
     }, [])
 
-    const data = {
-                labels: ['Concluido', 'Pendente', 'Andamento', 'Cancelado'],
-                datasets:[
-                    {
-                        label:'Nº de chamados',
-                        data: dataStatus,
-                        backgroundColor: [
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                        ],
-                        borderColor: [
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(54, 162, 235, 1)',
-                          ],
-                          borderWidth: 1,
-                    },
-                ],
-            };
+    console.log(dataChart)
 
     function getStatusData(data) {
         let concluido = 0
@@ -64,18 +71,33 @@ export default function PieChart() {
     }
     return(
         <>
-            <Pie data={data}
-            redraw={true}
-            options={{
-                plugins: {
-                    title: {
-                        display: true,
-                        text: "Status dos chamados",
-                        fontSize: 20
-                    }
-                }
-            }}
-            />
+        <div className="w-1/4 h-1/2 p-6  ring-1 ring-gray-900/5 shadow-lg rounded-xl flex flex-col items-center justify-center">
+            <h1 className="font-medium text-xl">Status dos chamados</h1>
+            {
+                // verifica se os dados estão setados antes de exibir o gráfico
+                dataStatus && dataChart?.datasets ? (
+                    <Pie data={dataChart}
+                    options={{
+                        plugins: {
+                            datalabels: {
+                                display: true,
+                            }
+                        },
+                    }}
+                    />
+                ) : 
+                // caso os dados não estejam setados, será exibida uma tela de carregamento
+                <>
+                <div className="flex gap-2 items-center justify-center  dark:bg-preto">
+                    <AiOutlineLoading3Quarters
+                        size={25}
+                        className="animate-spin text-gray-900 dark:text-gray-50"
+                    />
+                    <p className="text-gray-900 dark:text-gray-50"> Carregando...</p>
+                </div>
+            </>
+            }
+        </div>
         </>
     )
 
--- src/pages/DashBoard/index.jsx
+++ src/pages/DashBoard/index.jsx
@@ -8,9 +8,7 @@ export default function DashBoard () {
         <>
             <Header/>
             <div className="w-full h-screen flex items-center justify-center">
-                <div className="w-1/4 h-1/2 p-6 bg-slate-50 shadow-lg rounded-xl flex items-center justify-center">
-                    <PieChart/>
-                </div>
+                <PieChart/>
             </div>
             <Footer/>
         </>
