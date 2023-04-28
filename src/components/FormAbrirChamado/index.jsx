ec25dc3f4ae70ebe2cba59a258f127baf214c7ef
Taima <gustavotaima18@gmail.com>
2023-04-28 11:57:07 -0300

    Arrumando os formulários

--- src/components/FormAbrirChamado/index.jsx
+++ src/components/FormAbrirChamado/index.jsx
@@ -1,252 +1,283 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import api from "../../services/api";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import secureLocalStorage from "react-secure-storage";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function FormAbrirChamado() {
-  const id = JSON.parse(secureLocalStorage.getItem("Id"));
-  const [imagem, setImagem] = useState("");
-  const [abrirChamado, setAbrirChamado] = useState({
-    problema: "",
-    prioridade: "",
-    patrimonio: "",
-    setor: "",
-    descricao: "",
-    funcionario_id: id,
-  });
+	const id = JSON.parse(secureLocalStorage.getItem("Id"));
+	const [imagem, setImagem] = useState(null);
+	const formRef = useRef(null);
+	const [abrirChamado, setAbrirChamado] = useState({
+		problema: "",
+		prioridade: "",
+		patrimonio: "",
+		setor: "",
+		descricao: "",
+		funcionario_id: id,
+	});
 
-  // resetar o formulario após o envio dos dados
-  const resetForm = () => {
-    setAbrirChamado({
-      problema: "",
-      prioridade: "",
-      patrimonio: "",
-      setor: "",
-      descricao: "",
-      funcionario_id: id,
-    })
-    setImagem("")
-  }
+	const [loading, setLoading] = useState(false);
 
-  // salvar os dados na const abrirChamado
-  const changeChamado = (e) => {
-    setAbrirChamado({
-      ...abrirChamado,
-      [e.target.name]: e.target.value,
-    });
-  };
-  
-  // configuração do headers da api
-  const config = {
-    headers: { "content-type": "multipart/form-data" },
-  };
+	// resetar o formulario após o envio dos dados
+	const resetForm = () => {
+		setAbrirChamado({
+			problema: "",
+			prioridade: "",
+			patrimonio: "",
+			setor: "",
+			descricao: "",
+			funcionario_id: id,
+		});
+		setImagem(null);
+	};
 
-  const handleChamado = async (e) => {
-    e.preventDefault();
-    if (imagem) {
-      try {
-        let formData = new FormData();
-        formData.append("prioridade", abrirChamado.prioridade);
-        formData.append("patrimonio", abrirChamado.patrimonio);
-        formData.append("problema", abrirChamado.problema);
-        formData.append("setor", abrirChamado.setor);
-        formData.append("descricao", abrirChamado.descricao);
-        formData.append("anexo", imagem[0]);
-        formData.append("funcionario_id", id);
+	// salvar os dados na const abrirChamado
+	const changeChamado = (e) => {
+		setAbrirChamado({
+			...abrirChamado,
+			[e.target.name]: e.target.value,
+		});
+	};
 
-        const { data } = await api.post("/chamados/criar", formData, config);
-        toast.success(data.message, {
-          position: toast.POSITION.TOP_RIGHT,
-        });
-        resetForm()
-      } catch (error) {
-        toast.error(error.response.data.message, {
-          position: toast.POSITION.TOP_RIGHT,
-        });
-      }
-    } else {
-      try {
-        const { data } = await api.post("/chamados/criar", abrirChamado);
-        toast.success(data.message, {
-          position: toast.POSITION.TOP_RIGHT,
-        });
-        resetForm()
-      } catch (error) {
-        toast.error(error.response.data.message, {
-          position: toast.POSITION.TOP_RIGHT,
-        });
-      }
-    }
-  };
+	// configuração do headers da api
+	const config = {
+		headers: { "content-type": "multipart/form-data" },
+	};
 
-  return (
-    <>
-      <ToastContainer />
-      <h1 className="text-center text-gray-900 font-bold text-2xl sm:mt-4 dark:text-branco">
-        Abrir um chamado
-      </h1>
+	console.log(imagem);
 
-      <div className="mt-0 flex flex-col p-6 gap-y-5 lg:mt-12 lg:grid lg:grid-cols-2 lg:gap-x-10 lg:gap-y-2">
-        <div>
-          <label className="text-lg font-medium text-gray-900 dark:text-branco">
-            Problema *
-          </label>
-          <select
-            className="focus:outline-none focus:border-azul-hyde border-b-2 w-full p-2 dark:bg-transparent"
-            name="problema"
-            value={abrirChamado.problema}
-            onChange={changeChamado}
-            required
-          >
-            <option
-              className="text-gray-900 dark:text-branco dark:bg-gray-800"
-              selected
-              value=""
-            >
-              Selecione uma opção
-            </option>
-            <option
-              className="text-gray-900 dark:text-branco dark:bg-gray-800"
-              value="Hardware"
-            >
-              Hardware
-            </option>
-            <option
-              className="text-gray-900 dark:text-branco dark:bg-gray-800"
-              value="Infraestrutura"
-            >
-              Infraestrutura
-            </option>
-            <option
-              className="text-gray-900 dark:text-branco dark:bg-gray-800"
-              value="Software"
-            >
-              Software
-            </option>
-          </select>
-        </div>
+	const handleChamado = async (e) => {
+		e.preventDefault();
+		setLoading(true);
 
-        <div>
-          <label className="text-lg font-medium text-gray-900  dark:text-branco">
-            Prioridade *
-          </label>
-          <select
-            className="focus:outline-none focus:border-azul-hyde border-b-2 w-full p-2 dark:bg-transparent"
-            name="prioridade"
-            value={abrirChamado.prioridade}
-            onChange={changeChamado}
-            required
-          >
-            <option
-              className="text-gray-900 dark:text-branco dark:bg-gray-800"
-              selected
-              value=""
-            >
-              Selecione uma opção
-            </option>
-            <option
-              className="text-gray-900 dark:text-branco dark:bg-gray-800"
-              value="Alta"
-            >
-              Alta
-            </option>
-            <option
-              className="text-gray-900 dark:text-branco dark:bg-gray-800"
-              value="Média"
-            >
-              Média
-            </option>
-            <option
-              className="text-gray-900 dark:text-branco dark:bg-gray-800"
-              value="Baixa"
-            >
-              Baixa
-            </option>
-          </select>
-        </div>
+		if (imagem !== null) {
+			console.log("teste");
+			try {
+				let formData = new FormData();
+				formData.append("prioridade", abrirChamado.prioridade);
+				formData.append("patrimonio", abrirChamado.patrimonio);
+				formData.append("problema", abrirChamado.problema);
+				formData.append("setor", abrirChamado.setor);
+				formData.append("descricao", abrirChamado.descricao);
+				formData.append("anexo", imagem[0]);
+				formData.append("funcionario_id", id);
 
-        <div>
-          <label className="text-lg font-medium text-gray-900 dark:text-branco">
-            Patrimônio *
-          </label>
-          <input
-            className="focus:outline-none focus:border-azul-hyde border-b-2 w-full p-2 dark:bg-transparent"
-            placeholder="Informe o patrimônio"
-            value={abrirChamado.patrimonio}
-            onChange={changeChamado}
-            name="patrimonio"
-          />
-        </div>
-        <div>
-          <label className="text-lg font-medium text-gray-900 dark:text-branco">
-            Setor *
-          </label>
-          <input
-            className="focus:outline-none focus:border-azul-hyde border-b-2 w-full p-2 dark:bg-transparent"
-            placeholder="Informe o setor"
-            value={abrirChamado.setor}
-            onChange={changeChamado}
-            name="setor"
-          />
-        </div>
-        <div className="grid col-span-2">
-          <div>
-            <label className="text-lg font-medium text-gray-900 dark:text-branco">
-              Descrição
-            </label>
-            <input
-              className="focus:outline-none focus:border-azul-hyde border-b-2 w-full p-2 dark:bg-transparent"
-              placeholder="Descreva o seu problema"
-              value={abrirChamado.descricao}
-              onChange={changeChamado}
-              name="descricao"
-            />
-          </div>
-          <div className="mt-6 md:mt-1 ">
-            <div className="flex flex-row">
-              <label className="text-lg font-medium text-gray-900 dark:text-branco">
-                Anexo
-              </label>
-              <p
-                className="ml-3 mt-1 text-sm text-gray-500 dark:text-gray-300"
-                id="file_input_help"
-              >
-                PNG, JPG ou JPEG.
-              </p>
-            </div>
-            <input
-              type="file"
-              className="focus:outline-none focus:border-azul-hyde border-b-2 w-full  p-2 mb-2 dark:bg-transparent"
-              placeholder="Escolher arquivo"
-              name="anexo"
-              value=""
-              onChange={(e) => setImagem(e.target.files)}
-              accept=".png, .jpg, .jpeg"
-            />
-            {imagem ? (
-              <>
-                <img
-                  src={URL.createObjectURL(imagem[0])}
-                  alt="Foto de anexo"
-                  className="w-56 h-56"
-                />
-              </>
-            ) : (
-              ""
-            )}
-          </div>
-        </div>
-      </div>
+				const { data } = await api.post("/chamados/criar", formData, config);
+				setLoading(false);
+				toast.success(data.message, {
+					position: toast.POSITION.TOP_RIGHT,
+				});
+				resetForm();
+			} catch (error) {
+				setLoading(false);
+				toast.error(error.response.data.message, {
+					position: toast.POSITION.TOP_RIGHT,
+				});
+			}
+		} else {
+			try {
+				const { data } = await api.post("/chamados/criar", abrirChamado);
+				setLoading(false);
+				toast.success(data.message, {
+					position: toast.POSITION.TOP_RIGHT,
+				});
+				resetForm();
+			} catch (error) {
+				setLoading(false);
+				toast.error(error.response.data.message, {
+					position: toast.POSITION.TOP_RIGHT,
+				});
+			}
+		}
 
-      <div className=" mt-8 flex justify-center items-center flex-col">
-        <button
-          className="hover:bg-cyan-600 mb-6 bg-azul-hyde p-2 rounded-md text-white font-bold text-lg w-80"
-          onClick={handleChamado}
-        >
-          Enviar
-        </button>
-      </div>
-    </>
-  );
+		formRef.current.reset();
+		setImagem(null);
+	};
+
+	return (
+		<>
+			<ToastContainer />
+			<h1 className="text-center text-gray-900 font-bold text-2xl sm:mt-4 dark:text-branco">
+				Abrir um chamado
+			</h1>
+
+			<form encType="multipart/form" onSubmit={handleChamado} ref={formRef}>
+				<div className="mt-0 flex flex-col p-6 gap-y-5 lg:mt-12 lg:grid lg:grid-cols-2 lg:gap-x-10 lg:gap-y-2">
+					<div>
+						<label className="text-lg font-medium text-gray-900 dark:text-branco">
+							Problema *
+						</label>
+						<select
+							className="focus:outline-none focus:border-azul-hyde border-b-2 w-full p-2 dark:bg-transparent"
+							name="problema"
+							value={abrirChamado.problema}
+							onChange={changeChamado}
+							required
+						>
+							<option
+								className="text-gray-900 dark:text-branco dark:bg-gray-800"
+								selected
+							>
+								Selecione uma opção
+							</option>
+							<option
+								className="text-gray-900 dark:text-branco dark:bg-gray-800"
+								value="Hardware"
+							>
+								Hardware
+							</option>
+							<option
+								className="text-gray-900 dark:text-branco dark:bg-gray-800"
+								value="Infraestrutura"
+							>
+								Infraestrutura
+							</option>
+							<option
+								className="text-gray-900 dark:text-branco dark:bg-gray-800"
+								value="Software"
+							>
+								Software
+							</option>
+						</select>
+					</div>
+
+					<div>
+						<label className="text-lg font-medium text-gray-900  dark:text-branco">
+							Prioridade *
+						</label>
+						<select
+							className="focus:outline-none focus:border-azul-hyde border-b-2 w-full p-2 dark:bg-transparent"
+							name="prioridade"
+							value={abrirChamado.prioridade}
+							onChange={changeChamado}
+							required
+						>
+							<option
+								className="text-gray-900 dark:text-branco dark:bg-gray-800"
+								selected
+								value=""
+							>
+								Selecione uma opção
+							</option>
+							<option
+								className="text-gray-900 dark:text-branco dark:bg-gray-800"
+								value="Alta"
+							>
+								Alta
+							</option>
+							<option
+								className="text-gray-900 dark:text-branco dark:bg-gray-800"
+								value="Média"
+							>
+								Média
+							</option>
+							<option
+								className="text-gray-900 dark:text-branco dark:bg-gray-800"
+								value="Baixa"
+							>
+								Baixa
+							</option>
+						</select>
+					</div>
+
+					<div>
+						<label className="text-lg font-medium text-gray-900 dark:text-branco">
+							Patrimônio *
+						</label>
+						<input
+							className="focus:outline-none focus:border-azul-hyde border-b-2 w-full p-2 dark:bg-transparent"
+							placeholder="Informe o patrimônio"
+							value={abrirChamado.patrimonio}
+							onChange={changeChamado}
+							name="patrimonio"
+						/>
+					</div>
+					<div>
+						<label className="text-lg font-medium text-gray-900 dark:text-branco">
+							Setor *
+						</label>
+						<input
+							className="focus:outline-none focus:border-azul-hyde border-b-2 w-full p-2 dark:bg-transparent"
+							placeholder="Informe o setor"
+							value={abrirChamado.setor}
+							onChange={changeChamado}
+							name="setor"
+						/>
+					</div>
+					<div className="grid col-span-2">
+						<div>
+							<label className="text-lg font-medium text-gray-900 dark:text-branco">
+								Descrição
+							</label>
+							<input
+								className="focus:outline-none focus:border-azul-hyde border-b-2 w-full p-2 dark:bg-transparent"
+								placeholder="Descreva o seu problema"
+								value={abrirChamado.descricao}
+								onChange={changeChamado}
+								name="descricao"
+							/>
+						</div>
+						<div className="mt-6 md:mt-1 ">
+							<div className="flex flex-row">
+								<label className="text-lg font-medium text-gray-900 dark:text-branco">
+									Anexo
+								</label>
+								<p
+									className="ml-3 mt-1 text-sm text-gray-500 dark:text-gray-300"
+									id="file_input_help"
+								>
+									PNG, JPG ou JPEG.
+								</p>
+							</div>
+							<input
+								type="file"
+								className="focus:outline-none focus:border-azul-hyde border-b-2 w-full  p-2 mb-2 dark:bg-transparent"
+								placeholder="Escolher arquivo"
+								name="anexo"
+								onChange={(e) => setImagem(e.target.files)}
+								accept=".png, .jpg, .jpeg"
+							/>
+							{imagem !== null ? (
+								<>
+									{imagem.length !== 0 && (
+										<img
+											src={URL.createObjectURL(imagem[0])}
+											alt="Foto de anexo"
+											className="w-56 h-56"
+										/>
+									)}
+								</>
+							) : (
+								""
+							)}
+						</div>
+					</div>
+				</div>
+
+				<div className=" mt-8 flex justify-center items-center flex-col">
+					<button
+						className={`hover:bg-cyan-600 mb-6 bg-azul-hyde py-2 px-5 rounded-md text-white font-bold text-lg flex items-center justify-center gap-3 w-80 ${
+							loading ? "bg-cyan-600" : ""
+						}`}
+						disabled={loading}
+						type="submit"
+					>
+						{loading ? (
+							<>
+								<AiOutlineLoading3Quarters
+									size={28}
+									className="icon dark:text-gray-50"
+								/>
+								Processando...
+							</>
+						) : (
+							<>Enviar</>
+						)}
+					</button>
+				</div>
+			</form>
+		</>
+	);
 }
--- src/components/FormLogin/index.jsx
+++ src/components/FormLogin/index.jsx
@@ -3,154 +3,175 @@ import { useState, useContext, useRef } from "react";
 import { Context } from "../../context/AuthContext";
 import InputMask from "react-input-mask";
 import { ToastContainer } from "react-toastify";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 function Form() {
-  const [label, setLabel] = useState("cpf");
-  const [usuario, setUsuario] = useState("");
-  const senha = useRef();
-  const { handleLogin, status } = useContext(Context);
-  var user = {};
-  const getRef = () => {
-    if (label === "usuario") {
-      return (user = {
-        usuario: usuario,
-        senha: senha.current.value,
-      });
-    } else {
-      return (user = {
-        [label]: usuario.replace(/[^0-9]+/g, ""),
-        senha: senha.current.value,
-      });
-    }
-  };
+	const [label, setLabel] = useState("cpf");
+	const [usuario, setUsuario] = useState("");
+	const senha = useRef();
+	const { handleLogin, status } = useContext(Context);
+	var user = {};
+	const getRef = () => {
+		if (label === "usuario") {
+			return (user = {
+				usuario: usuario,
+				senha: senha.current.value,
+			});
+		} else {
+			return (user = {
+				[label]: usuario.replace(/[^0-9]+/g, ""),
+				senha: senha.current.value,
+			});
+		}
+	};
 
-  const handleChange = (event) => {
-    setLabel(event.target.value);
-  };
+	const [loading, setLoading] = useState(false);
 
-  return (
-    <div className="bg-white px-10 py-10 dark:bg-preto">
-      <div className="sm:px-0 sm:shrink lg:px-8">
-        <p className="font-semibold text-lg dark:text-branco">Entrar como:</p>
-        <div className="flex flex-row">
-          <div className="flex flex-row items-center">
-            <input
-              type="radio"
-              id="cpf"
-              className="w-4 h-4 text-azul-hyde bg-gray-100  border-gray-300 focus:azul-hyde dark:focus:azul-hyde dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              name="escolhalogin"
-              value="cpf"
-              onChange={handleChange}
-              defaultChecked
-            />
-            <label
-              htmlFor="cpf"
-              className="mr-4 ml-2 font-semibold dark:text-branco"
-            >
-              Técnico
-            </label>
-          </div>
-          <div className="flex flex-row items-center">
-            <input
-              type="radio"
-              id="cnpj"
-              className="w-4 h-4 text-azul-hyde bg-gray-100 border-gray-300 focus:azul-hyde dark:focus:azul-hyde dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              name="escolhalogin"
-              value="cnpj"
-              onChange={handleChange}
-            />
-            <label
-              htmlFor="cnpj"
-              className="mr-4 ml-2 font-semibold dark:text-branco"
-            >
-              Empresa
-            </label>
-          </div>
-          <div className="flex flex-row items-center">
-            <input
-              type="radio"
-              id="matricula"
-              className="w-4 h-4 text-azul-hyde bg-gray-100 border-gray-300 focus:azul-hyde dark:focus:azul-hyde dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              name="escolhalogin"
-              value="usuario"
-              onChange={handleChange}
-            />
-            <label
-              htmlFor="matricula"
-              className="ml-2 font-semibold dark:text-branco"
-            >
-              Funcionário
-            </label>
-          </div>
-        </div>
-      </div>
-      <div className="mt-8 mb-3">
-        <label className="text-lg font-semibold  text-gray-900 dark:text-branco">
-          Login
-        </label>
-        <InputMask
-          className="focus:outline-none focus:border-azul-hyde dark:bg-preto dark:text-branco border-b-2 w-full p-2"
-          placeholder={
-            label === "cpf" || label === "cnpj"
-              ? label.toUpperCase()
-              : "Usuário"
-          }
-          autoCapitalize="false"
-          name="cpf"
-          value={usuario}
-          onChange={(e) => setUsuario(e.target.value)}
-          mask={
-            label === "cpf"
-              ? "999.999.999-99"
-              : label === "cnpj"
-              ? "99.999.999/9999-99"
-              : label === "matricula"
-              ? ""
-              : ""
-          }
-          required
-        />
-      </div>
-      <div>
-        <label className="text-lg font-semibold  text-gray-900 dark:text-branco">
-          Senha
-        </label>
-        <input
-          type="password"
-          autoCapitalize="false"
-          className="focus:outline-none focus:border-azul-hyde dark:bg-preto dark:text-branco  border-b-2 w-full p-2"
-          ref={senha}
-          placeholder="Senha"
-        />
-      </div>
-      <div className="mt-8 flex flex-col">
-        <button
-          className="hover:bg-cyan-600 mb-6 bg-azul-hyde p-2 rounded-md text-white font-bold text-lg "
-          onClick={(e) => {
-            e.key === "Enter" || e.keyCode === 13
-              ? handleLogin(getRef(), label)
-              : handleLogin(getRef(), label);
-          }}
-        >
-          {" "}
-          Login
-        </button>
-        <ToastContainer />
-        <p className="text-red-500 flex justify-center">{status}</p>
+	const handleChange = (event) => {
+		setLabel(event.target.value);
+	};
 
-        <Link className="no-underline flex " to="/recuperar">
-          <p className="dark:text-branco font-semibold ">Esqueceu a senha?</p>
-          <p className="ml-2 text-azul-hyde font-semibold">Recuperar</p>
-        </Link>
-        <Link className="no-underline flex  " to="/cadastro">
-          <p className="dark:text-branco font-semibold mb-1">
-            Não possui uma conta?
-          </p>
-          <p className="ml-2 text-azul-hyde font-semibold mb-1">Cadastrar</p>
-        </Link>
-      </div>
-    </div>
-  );
+	const login = async (event) => {
+		event.preventDefault();
+		setLoading(true);
+		await handleLogin(getRef(), label);
+		setLoading(false);
+	};
+
+	return (
+		<div className="bg-white px-10 py-10 dark:bg-preto">
+			<div className="sm:px-0 sm:shrink lg:px-8">
+				<p className="font-semibold text-lg dark:text-branco">Entrar como:</p>
+				<div className="flex flex-row">
+					<div className="flex flex-row items-center">
+						<input
+							type="radio"
+							id="cpf"
+							className="w-4 h-4 text-azul-hyde bg-gray-100  border-gray-300 focus:azul-hyde dark:focus:azul-hyde dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							name="escolhalogin"
+							value="cpf"
+							onChange={handleChange}
+							defaultChecked
+						/>
+						<label
+							htmlFor="cpf"
+							className="mr-4 ml-2 font-semibold dark:text-branco"
+						>
+							Técnico
+						</label>
+					</div>
+					<div className="flex flex-row items-center">
+						<input
+							type="radio"
+							id="cnpj"
+							className="w-4 h-4 text-azul-hyde bg-gray-100 border-gray-300 focus:azul-hyde dark:focus:azul-hyde dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							name="escolhalogin"
+							value="cnpj"
+							onChange={handleChange}
+						/>
+						<label
+							htmlFor="cnpj"
+							className="mr-4 ml-2 font-semibold dark:text-branco"
+						>
+							Empresa
+						</label>
+					</div>
+					<div className="flex flex-row items-center">
+						<input
+							type="radio"
+							id="matricula"
+							className="w-4 h-4 text-azul-hyde bg-gray-100 border-gray-300 focus:azul-hyde dark:focus:azul-hyde dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							name="escolhalogin"
+							value="usuario"
+							onChange={handleChange}
+						/>
+						<label
+							htmlFor="matricula"
+							className="ml-2 font-semibold dark:text-branco"
+						>
+							Funcionário
+						</label>
+					</div>
+				</div>
+			</div>
+
+			<form onSubmit={login}>
+				<div className="mt-8 mb-3">
+					<label className="text-lg font-semibold  text-gray-900 dark:text-branco">
+						Login
+					</label>
+					<InputMask
+						className="focus:outline-none focus:border-azul-hyde dark:bg-preto dark:text-branco border-b-2 w-full p-2"
+						placeholder={
+							label === "cpf" || label === "cnpj"
+								? label.toUpperCase()
+								: "Usuário"
+						}
+						autoCapitalize="false"
+						name="cpf"
+						value={usuario}
+						onChange={(e) => setUsuario(e.target.value)}
+						mask={
+							label === "cpf"
+								? "999.999.999-99"
+								: label === "cnpj"
+								? "99.999.999/9999-99"
+								: label === "matricula"
+								? ""
+								: ""
+						}
+						required
+					/>
+				</div>
+				<div>
+					<label className="text-lg font-semibold  text-gray-900 dark:text-branco">
+						Senha
+					</label>
+					<input
+						type="password"
+						autoCapitalize="false"
+						className="focus:outline-none focus:border-azul-hyde dark:bg-preto dark:text-branco  border-b-2 w-full p-2"
+						ref={senha}
+						placeholder="Senha"
+					/>
+				</div>
+				<div className="mt-8 flex flex-col">
+					<button
+						className={`hover:bg-cyan-600 mb-6 bg-azul-hyde p-2 rounded-md text-white font-bold text-lg flex items-center justify-center gap-3 ${
+							loading ? "bg-cyan-600" : ""
+						}`}
+						disabled={loading}
+						type="submit"
+					>
+						{loading ? (
+							<>
+								<AiOutlineLoading3Quarters
+									size={28}
+									className="icon dark:text-gray-50"
+								/>
+								Processando...
+							</>
+						) : (
+							<>Login</>
+						)}
+					</button>
+					<ToastContainer />
+					<p className="text-red-500 flex justify-center">{status}</p>
+
+					<Link className="no-underline flex " to="/recuperar">
+						<p className="dark:text-branco font-semibold ">Esqueceu a senha?</p>
+						<p className="ml-2 text-azul-hyde font-semibold">Recuperar</p>
+					</Link>
+					<Link className="no-underline flex  " to="/cadastro">
+						<p className="dark:text-branco font-semibold mb-1">
+							Não possui uma conta?
+						</p>
+						<p className="ml-2 text-azul-hyde font-semibold mb-1">Cadastrar</p>
+					</Link>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default Form;
