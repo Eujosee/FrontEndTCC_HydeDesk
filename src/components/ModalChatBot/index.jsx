617782ef1f35d4ffa4be4451a5d3d844326f30f8
Eujosee <josemotadossantosjunior@hotmail.com>
2023-05-04 10:34:15 -0300

    arrumando o chatbot, os botões estão bugados

--- src/components/ModalChatBot/index.jsx
+++ src/components/ModalChatBot/index.jsx
@@ -1,13 +1,18 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment } from "react";
-import Iframe from "react-iframe";
 import { GrFormClose } from "react-icons/gr";
+import './index.css'
 
 export default function ModalChatBot({ open, onClose }) {
   if (!open) return null;
   const styleOptions = {
     // Add styleOptions to customize Web Chat canvas
     hideUploadButton: true,
+    botAvatarInitials: 'HD',
+            accent: '#00809d',
+            botAvatarBackgroundColor: "#FFFFFF",
+            botAvatarImage: 'https://learn.microsoft.com/azure/bot-service/v4sdk/media/logo_bot.svg',
+            userAvatarImage: 'https://avatars.githubusercontent.com/u/661465'
   };
 
   var theURL =
@@ -17,9 +22,7 @@ export default function ModalChatBot({ open, onClose }) {
     0,
     theURL.indexOf("/powervirtualagents")
   );
-  var apiVersion = theURL
-    .slice(theURL.indexOf("api-version"))
-    .split("=")[1];
+  var apiVersion = theURL.slice(theURL.indexOf("api-version")).split("=")[1];
   var regionalChannelSettingsURL = `${environmentEndPoint}/powervirtualagents/regionalchannelsettings?api-version=${apiVersion}`;
 
   var directline;
@@ -35,10 +38,8 @@ export default function ModalChatBot({ open, onClose }) {
   const store = window.WebChat.createStore(
     {},
     ({ dispatch }) =>
-    
       (next) =>
       (action) => {
-        console.log('oi')
         if (action.type === "DIRECT_LINE/CONNECT_FULFILLED") {
           dispatch({
             meta: {
@@ -103,45 +104,15 @@ export default function ModalChatBot({ open, onClose }) {
               leaveTo="opacity-0 scale-95"
             >
               <div className="fixed inset-0 z-10">
-                <Dialog.Panel  className="fixed hidden md:flex inset-y-0 right-0 z-10 bg-white dark:bg-gray-800 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 ">
-                   <div id="webchat" role="main"></div>
-                </Dialog.Panel>
-
-                {/* MOBILE */}
-
-                <div className="fixed inset-0 overflow-y-auto">
-                  <div className="flex min-h-full items-center justify-center text-center">
-                    <Transition.Child
-                      as={Fragment}
-                      enter="ease-out duration-300"
-                      enterFrom="opacity-0 scale-95"
-                      enterTo="opacity-100 scale-100"
-                      leave="ease-in duration-200"
-                      leaveFrom="opacity-100 scale-100"
-                      leaveTo="opacity-0 scale-95"
-                    >
-                      <Dialog.Panel className="transform overflow-hidden md:hidden bg-white  shadow-xl transition-all">
-                        <div className="">
-                          <button
-                            type="button"
-                            className="w-full flex justify-center items-center m-0 md:hidden border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                            onClick={onClose}
-                          >
-                            <GrFormClose size={30} className="text-white" />
-                          </button>
-                          <Iframe
-                            url="https://web.powerva.microsoft.com/environments/Default-b1051c4b-3b94-41ab-9441-e73a72342fdd/bots/new_bot_3e41221dd72148d4b5c14612de784c07/webchat"
-                            width="540px"
-                            height="720px"
-                            className="bg-white w-screen h-screen"
-                            display="block"
-                            position="relative"
-                          />
-                        </div>
-                      </Dialog.Panel>
-                    </Transition.Child>
+                <Dialog.Panel className="fixed flex flex-col inset-y-0 right-0 z-10 bg-white dark:bg-gray-800 max-w-full md:max-w-md sm:ring-1 sm:ring-gray-900/10 ">
+                  <div className="p-5 bg-azul-hyde flex flex-row items-center relative">
+                    <button onClick={onClose} className="flex md:hidden justify-start absolute bg-white rounded-full p-2">
+                      <GrFormClose size={25}/>
+                    </button>
+                    <h1 className="text-white font-bold text-center w-full text-lg">Hydezinho</h1>
                   </div>
-                </div>
+                  <div id="webchat" role="main" className="w-full h-full text-justify tracking-normal text-md focus:outline-none"></div>
+                </Dialog.Panel>
               </div>
             </Transition.Child>
           </div>
