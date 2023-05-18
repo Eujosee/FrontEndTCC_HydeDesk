0234afc144ce86bbf99f1ee717edf462df7faece
Taima <gustavotaima18@gmail.com>
2023-05-18 09:24:48 -0300

    Arrumando url

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log(import.meta.env.VITE_REACT_API)
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: import.meta.env.VITE_REACT_API,
 });
 
 export default api;
